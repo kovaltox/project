3b2750fb974a32f88e27ed6af2a0014d3da4ad28
Alexandr <tox@nextmail.ru>
2025-09-06 22:46:32 +0300

    First commit

---
+++ js/script.js
@@ -0,0 +1,64 @@
+// "use strict";
+
+// // let numder = 5;
+// // // console.log(4/0);
+
+// // const obj = {
+// // name:"Alex",
+// // age: 25,
+// // isMarried: false
+
+// // }
+// // console.log(obj.name);
+
+// // let arr = [
+// //   'plum.png', 'orange.png', 'apple.bmp', 5
+// // ]
+// // console.log(arr[1]);
+
+// const arr = [1, 2, 3];
+
+// const arrObj = {
+//   a: 1,
+//   1: 2,
+//   2: 3,
+// };
+
+// arrObj.b = '1112'
+// arrObj['b'] = 1111
+
+// console.log(arrObj['b']);
+// console.log(arrObj.b);
+// // const obj = { a: 1, b: 2 };
+// const obj = {
+//   Anna: 500,
+//   Alice: 800,
+// };
+
+// alert('sdfsdffs')
+
+// const result = confirm('Ты тут?')
+// console.log(result);
+
+// const answer = +prompt('Вам есть 18?', '18')
+// console.log(answer +5);
+
+// const answers= [];
+// answers[0] = prompt('Как ваше имя?', '')
+// answers[1] = prompt('Как ваше фамилия?', '')
+// answers[0] = prompt('Сколько вам лет?', '')
+// document.write(answers)
+
+let incr = 10,
+  decr = 10;
+
+// ++incr
+// --decr
+console.log(incr++, decr--);
+console.log(incr++, decr--);
+console.log(++incr, --decr);
+
+const isChecked = true,
+      isClose = true;
+
+        console.log(isChecked&&isClose);
